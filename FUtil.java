2211341869afe2ceeecca520978563f244c8643e
AdrianL-1011 <adrianlcamps06@gmail.com>
2025-05-10 11:51:48 +0200

    Adrian Primer Commit

--- FUtil.java
+++ FUtil.java
@@ -1,6 +1,7 @@
 package es.mdc;
 import java.io.File;
 /**
+ * @author Adrián
  * @param filename  es la  ruta del archivo que se va a validar.
  * @return  si la ruta es correcta
  * @throws para comprobar si la ruta esta vacía o solo contiene espacios.
